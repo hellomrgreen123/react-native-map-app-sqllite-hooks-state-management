5122b8399ac44d3b500001be40af5bb4b4e10acc
hellomrgreen123 <oisinomaolchallan@gmail.com>
2020-10-25 16:53:36 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import { init } from './helpers/db';
 import PlacesNavigator from './navigation/PlacesNavigator';
+import configureStore from './hooks/stores/placesStore'
 
+configureStore()
+
+init().then(()=>{
+  console.log('Initialized database')
+}).catch(err=>{
+  console.log('Initializing db failed.')
+  console.log(err)
+})
 export default function App() {
   return <PlacesNavigator />;
 }
