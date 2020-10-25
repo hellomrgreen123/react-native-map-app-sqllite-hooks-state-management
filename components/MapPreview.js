53b44ba4701dada24ec22b84992992aa4792c4c1
hellomrgreen123 <oisinomaolchallan@gmail.com>
2020-10-25 16:54:24 +0100

    Update MapPreview.js

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -8,12 +8,8 @@ const MapPreview = props => {
 
     let imagePreviewUrl
     if (props.location) {
-        imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat
-            },${props.location.lng
-            }&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat
-            },${props.location.lng}&key=${variables.googleApiKey}`;
+        imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${variables.googleApiKey}`;
     }
-
     return <TouchableOpacity onPress={props.onPress} style={{ ...styles.mapPreview, ...props.style }}>
         {props.location ? <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} /> : props.children}
     </TouchableOpacity>
